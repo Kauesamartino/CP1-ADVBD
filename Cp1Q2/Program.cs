92aa6def59ea3b1aca53c8f4f099c860335e2cc6
davixedes <davipraxedes10@gmail.com>
2025-09-27 00:26:35 -0300

    Commitando sprint

---
+++ Cp1Q2/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Insira aqui o seu nome: ");
+var nome = Console.ReadLine();
+
+Console.Write("Insira aqui o dia de seu nascimento (dd/mm/yyyy): ");
+var diaNascimento= Convert.ToDateTime(Console.ReadLine());
+
+DateTime hoje = DateTime.Now; 
+
+var idade = hoje - diaNascimento;
+
+var idadeEmAnos = idade.Days / 365;
+
+Console.WriteLine("Voce tem " + idadeEmAnos + " Anos");
+
+if (idadeEmAnos < 16)
+{
+    Console.WriteLine("Voce nao pode tirar carteira de motorista, vai estudar! ");
+} else if (idadeEmAnos >= 16 && idadeEmAnos < 18)
+{
+    Console.Write("Voce ja é emancipado(a) (true or false) ");
+    bool isEmancipado = Convert.ToBoolean(Console.ReadLine());
+    if (isEmancipado)
+    {
+        Console.WriteLine("Vocepode tirar a carteira de motorista, só não dirija um kwid: ");
+    }
+    else
+    {
+        Console.WriteLine("Voce nao pode tirar motorista ");
+    }
+}
+else
+{
+    Console.WriteLine(nome + ", Voce pode tirar carteira, só não beba enquanto dirija");
+}
